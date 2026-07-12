d0e395f1973bb87e95e7cb0c7f502f21e44b02a5
goulili75 <goulili75@users.noreply.github.com>
2026-07-12 15:56:53 +0800

    test: v3 - add UIDevice hooks

--- QNByPass/Tweak_Compiled.m
+++ QNByPass/Tweak_Compiled.m
@@ -1,8 +1,26 @@
-// Step 1: just initWith the basic
+// v3: + UIKit + UIDevice hooks
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <objc/runtime.h>
 #import "substrate.h"
+
+static BOOL hookNo(id _s, SEL _c, ...) { return NO; }
 
 __attribute__((constructor))
 static void init(void) {
-    NSLog(@"[QNByPass] v2 - testing...");
+    NSLog(@"[QNByPass] v3 loading...");
+    
+    Class c = NSClassFromString(@"UIDevice");
+    if (c) {
+        SEL sl[] = {
+            @selector(isJailbroken),@selector(isJailBreak),@selector(isJailBroken),
+            @selector(isDeviceJailbroken),@selector(checkJailbroken)
+        };
+        for (int i = 0; i < sizeof(sl)/sizeof(SEL); i++) {
+            Method m = class_getInstanceMethod(c, sl[i]);
+            if (m) class_replaceMethod(c, sl[i], (IMP)hookNo, method_getTypeEncoding(m));
+        }
+    }
+    
+    NSLog(@"[QNByPass] v3 loaded OK!");
 }
