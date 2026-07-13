99540f2841165bfa5a7e127131b1f2030918291e
goulili75 <goulili75@users.noreply.github.com>
2026-07-13 13:49:31 +0800

    fix: rpath + UIApplicationMain

--- QNByPass/App/main.m
+++ QNByPass/App/main.m
@@ -1,23 +1,23 @@
-// main.m - 极简入口，无AppDelegate
+// main.m
 #import <UIKit/UIKit.h>
 
-@interface V : UIViewController @end
-@implementation V
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    self.view.backgroundColor = [UIColor systemBackgroundColor];
-    UILabel *l = [[UILabel alloc] initWithFrame:CGRectMake(20, 100, 300, 50)];
-    l.text = @"QNByPass v3.2 - 正常!"; l.font = [UIFont boldSystemFontOfSize:20]; l.textAlignment = NSTextAlignmentCenter;
-    [self.view addSubview:l];
+@interface AppDel : UIResponder <UIApplicationDelegate>
+@property UIWindow *window;
+@end
+@implementation AppDel
+- (BOOL)application:(UIApplication *)app didFinishLaunchingWithOptions:(NSDictionary *)opt {
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor = [UIColor whiteColor];
+    UILabel *l = [[UILabel alloc] initWithFrame:CGRectMake(20,100,300,50)];
+    l.text = @"QNByPass OK!"; l.textAlignment = NSTextAlignmentCenter;
+    [self.window addSubview:l];
+    [self.window makeKeyAndVisible];
+    return YES;
 }
 @end
 
 int main(int argc, char *argv[]) {
     @autoreleasepool {
-        UIWindow *w = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-        w.rootViewController = [[V alloc] init];
-        [w makeKeyAndVisible];
-        // 保持事件循环
-        [[NSRunLoop currentRunLoop] run];
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDel class]));
     }
 }
