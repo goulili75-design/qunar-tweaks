03488812583974df1274d2b56751b9d9fc39890f
goulili75 <goulili75@users.noreply.github.com>
2026-07-13 18:13:35 +0800

    #85: standalone main.m + fPIE

---
+++ App_main_backup.m
@@ -0,0 +1,9 @@
+// main.m - QNByPass App 入口
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
--- QNByPass/App/main.m
+++ QNByPass/App/main.m
@@ -1,9 +1,23 @@
-// main.m - QNByPass App 入口
+// main.m
 #import <UIKit/UIKit.h>
-#import "AppDelegate.h"
+
+@interface AppDel : UIResponder <UIApplicationDelegate>
+@property UIWindow *window;
+@end
+@implementation AppDel
+- (BOOL)application:(UIApplication *)app didFinishLaunchingWithOptions:(NSDictionary *)opt {
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor = [UIColor whiteColor];
+    UILabel *l = [[UILabel alloc] initWithFrame:CGRectMake(20,100,300,50)];
+    l.text = @"QNByPass OK!"; l.textAlignment = NSTextAlignmentCenter;
+    [self.window addSubview:l];
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+@end
 
 int main(int argc, char *argv[]) {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDel class]));
     }
 }
